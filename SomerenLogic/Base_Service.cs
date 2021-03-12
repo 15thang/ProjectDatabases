54523a8881f8d9dfb7712eed6c1ce2f95e8b1117
neburpoots <neburpoots@hotmail.com>
2021-03-12 18:39:01 +0100

    Base service change

--- SomerenLogic/Base_Service.cs
+++ SomerenLogic/Base_Service.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿using SomerenDAL;
+using SomerenModel;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SomerenLogic
 {
-    class Base_Service
+    public class Base_Service
     {
+        public bool Error { get; set; }
+        public string ErrorText { get; set; }
     }
 }
